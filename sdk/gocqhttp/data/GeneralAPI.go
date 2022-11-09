ba8d08f9c863f6f1f9c466ec5b4f959411e1e471
manyue <manyue@elyart.cn>
2022-11-10 01:20:27 +0800

    Add more SDK data

--- sdk/gocqhttp/data/GeneralAPI.go
+++ sdk/gocqhttp/data/GeneralAPI.go
@@ -120,3 +120,238 @@ type SetGroupSpacialTitleData struct {
 type SetGroupSignData struct {
 	GroupId int `json:"group_id"`
 }
+
+// SetFriendAddRequestData Endpoint set_friend_add_request
+type SetFriendAddRequestData struct {
+	Flag    string `json:"flag"`
+	Approve bool   `json:"approve"`
+	Remark  string `json:"remark"`
+}
+
+// SetGroupAddRequestData Endpoint set_group_add_request
+type SetGroupAddRequestData struct {
+	Flag string `json:"flag"`
+	Type string `json:"type"`
+}
+
+// GetLoginInfoData Endpoint get_login_info
+type GetLoginInfoData struct {
+	UserID   int    `json:"user_id"`
+	Nickname string `json:"nickname"`
+}
+
+// SetQQProfileData Endpoint set_qq_profile
+type SetQQProfileData struct {
+	Nickname     string `json:"nickname"`
+	Company      string `json:"company"`
+	Email        string `json:"email"`
+	College      string `json:"college"`
+	PersonalNote string `json:"personal_note"`
+}
+
+// GetStrangerInfoData Endpoint get_stranger_info
+type GetStrangerInfoData struct {
+	UserId  int  `json:"user_id"`
+	NoCache bool `json:"no_cache"`
+}
+
+// GetFriendListData Endpoint get_friend_list
+type GetFriendListData struct {
+	UserID   int    `json:"user_id"`
+	Nickname string `json:"nickname"`
+	Remark   string `json:"remark"`
+}
+
+// GetUnidirectionalFriendListData Endpoint get_unidirectional_friend_list
+type GetUnidirectionalFriendListData struct {
+	UserID   int    `json:"user_id"`
+	Nickname string `json:"nickname"`
+	Source   string `json:"source"`
+}
+
+// DeleteFriendData Endpoint delete_friend
+type DeleteFriendData struct {
+	FriendID int `json:"friend_id"`
+}
+
+// GetGroupInfoData Endpoint get_group_info
+type GetGroupInfoData struct {
+	GroupId int  `json:"group_id"`
+	NoCache bool `json:"no_cache"`
+}
+
+// GetGroupMemberInfoData Endpoint get_group_member_info
+type GetGroupMemberInfoData struct {
+	GroupId int  `json:"group_id"`
+	UserId  int  `json:"user_id"`
+	NoCache bool `json:"no_cache"`
+}
+
+// GetGroupMemberListData Endpoint get_group_member_list
+type GetGroupMemberListData struct {
+	GroupId int  `json:"group_id"`
+	NoCache bool `json:"no_cache"`
+}
+
+// GetGroupHonorInfoData Endpoint get_group_honor_info
+type GetGroupHonorInfoData struct {
+	GroupId int    `json:"group_id"`
+	Type    string `json:"type"`
+}
+
+// SetRestartData Endpoint set_restart
+type SetRestartData struct {
+	Delay int `json:"delay"`
+}
+
+// SetGroupPortraitData Endpoint set_group_portrait
+type SetGroupPortraitData struct {
+	GroupId int    `json:"group_id"`
+	File    string `json:"file"`
+	Cache   int    `json:"cache"`
+}
+
+// GetWordSlicesData Endpoint .get_word_slices
+type GetWordSlicesData struct {
+	Content string `json:"content"`
+}
+
+// OCRImageData Endpoint ocr_image
+type OCRImageData struct {
+	Image string `json:"image"`
+}
+
+// UploadPrivateFileData Endpoint upload_private_file
+type UploadPrivateFileData struct {
+	UserId int    `json:"user_id"`
+	File   string `json:"file"`
+	Name   string `json:"name"`
+}
+
+// UploadGroupFileData Endpoint upload_group_file
+type UploadGroupFileData struct {
+	GroupId int    `json:"group_id"`
+	File    string `json:"file"`
+	Name    string `json:"name"`
+	Folder  string `json:"folder"`
+}
+
+// GetGroupFileSystemInfoData Endpoint get_group_file_system_info
+type GetGroupFileSystemInfoData struct {
+	GroupId int `json:"group_id"`
+}
+
+// GetGroupRootFilesData Endpoint get_group_root_files
+type GetGroupRootFilesData struct {
+	GroupId int `json:"group_id"`
+}
+
+// GetGroupFilesByFolderData Endpoint get_group_files_by_folder
+type GetGroupFilesByFolderData struct {
+	GroupId  int    `json:"group_id"`
+	FolderID string `json:"folder_id"`
+}
+
+// CreateGroupFileFolderData Endpoint create_group_file_folder
+type CreateGroupFileFolderData struct {
+	GroupId  int    `json:"group_id"`
+	Name     string `json:"name"`
+	ParentID string `json:"parent_id"`
+}
+
+// DeleteGroupFolderData Endpoint delete_group_folder
+type DeleteGroupFolderData struct {
+	GroupId  int    `json:"group_id"`
+	FolderID string `json:"folder_id"`
+}
+
+// DeleteGroupFileData Endpoint delete_group_file
+type DeleteGroupFileData struct {
+	GroupId int    `json:"group_id"`
+	FileID  string `json:"file_id"`
+	BusID   int    `json:"busid"`
+}
+
+// GetGroupFileUrlData Endpoint get_group_file_url
+type GetGroupFileUrlData struct {
+	GroupId int    `json:"group_id"`
+	FileID  string `json:"file_id"`
+	BusID   int    `json:"busid"`
+}
+
+// GetGroupAtAllRemainData Endpoint get_group_at_all_remain
+type GetGroupAtAllRemainData struct {
+	GroupId int `json:"group_id"`
+}
+
+// SendGroupNoticeData Endpoint _send_group_notice
+type SendGroupNoticeData struct {
+	GroupId int    `json:"group_id"`
+	Content string `json:"content"`
+	Image   string `json:"image"`
+}
+
+// GetGroupNoticeData Endpoint _get_group_notice
+type GetGroupNoticeData struct {
+	GroupId int `json:"group_id"`
+}
+
+// DownloadFileData Endpoint download_file
+type DownloadFileData struct {
+	Url         string `json:"url"`
+	ThreadCount int    `json:"thread_count"`
+	Headers     string `json:"headers"`
+}
+
+// GetOnlineClientsData Endpoint get_online_clients
+type GetOnlineClientsData struct {
+	NoCache bool `json:"no_cache"`
+}
+
+// GetGroupMsgHistoryData Endpoint get_group_msg_history
+type GetGroupMsgHistoryData struct {
+	MessageSeq int `json:"message_seq"`
+	GroupID    int `json:"group_id"`
+}
+
+// SetEssenceMsgData Endpoint set_essence_msg
+type SetEssenceMsgData struct {
+	MessageID int `json:"message_id"`
+}
+
+// DeleteEssenceMsgData Endpoint delete_essence_msg
+type DeleteEssenceMsgData struct {
+	MessageID int `json:"message_id"`
+}
+
+// GetEssenceMsgListData Endpoint get_essence_msg_list
+type GetEssenceMsgListData struct {
+	GroupID int `json:"group_id"`
+}
+
+// CheckUrlSafelyData Endpoint check_url_safely
+type CheckUrlSafelyData struct {
+	Url string `json:"url"`
+}
+
+// GetModelShowData Endpoint _get_model_show
+type GetModelShowData struct {
+	Model string `json:"model"`
+}
+
+// SetModelShowData Endpoint _set_model_show
+type SetModelShowData struct {
+	Model     string `json:"model"`
+	ModelShow string `json:"model_show"`
+}
+
+// DeleteUnidirectionalFriendData Endpoint delete_unidirectional_friend
+type DeleteUnidirectionalFriendData struct {
+	UserID int `json:"user_id"`
+}
+
+// SendPrivateForwardMsgData Endpoint send_private_forward_msg
+type SendPrivateForwardMsgData struct {
+	UserID   int    `json:"user_id"`
+	Messages string `json:"messages"`
+}
