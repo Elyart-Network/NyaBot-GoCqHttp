3781896d8c9a49492755caee9bb3b694d2907958
Season.Shi <seasonshi@elyart.dev>
2022-11-19 02:00:41 +0800

    Separate Steps

--- .space.kts
+++ .space.kts
@@ -6,13 +6,23 @@
 
 job("Build & Test") {
     container(displayName = "GoLang", image = "golang:1.19.2") {
-        shellScript {
+        shellScript { // Set Go Proxy
             interpreter = "/bin/bash"
             content = """
                 export GO111MODULE=on
                 export GOPROXY=https://goproxy.cn
+            """
+        }
+        shellScript { // Download Go Modules
+            interpreter = "/bin/bash"
+            content = """
                 go mod tidy
                 go mod download
+            """
+        }
+        shellScript { // Build Project
+            interpreter = "/bin/bash"
+            content = """
                 go build -o nyabot-gocqhttp
             """
         }
